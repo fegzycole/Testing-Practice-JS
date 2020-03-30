d88516f1e495b5d04fd5177739f3154d7f1d342c
fegzycole <fergusoniyara@gmail.com>
2020-03-30 22:51:16 +0100

    Update Readme documentation

--- arrayanalysis/arrayanalysis.js
+++ arrayanalysis/arrayanalysis.js
@@ -1,13 +1,17 @@
 const arrayAnalysis = (array) => {
   let min = array[0];
   let max = array[0];
+
   const { length } = array;
+
   let sum = 0;
+
   array.forEach((el) => {
     if (el < min) min = el;
     if (el > max) max = el;
     sum += el;
   });
+
   return {
     min, max, average: sum / length, length,
   };
